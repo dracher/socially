d839342efe7920f9232ac2ef770bc5ca9f0de4ad
dracher <dracher@gmail.com>
2015-10-30 00:07:21 +0800

    first commit

---
+++ app.ng.js
@@ -0,0 +1,7 @@
+if (Meteor.isClient) {
+	angular.module('socially', ['angular-meteor']);
+
+	angular.module('socially').controller('PartiesListCtrl', function($scope, $meteor) {
+		$scope.parties = $meteor.collection(Parties);
+	});
+}
